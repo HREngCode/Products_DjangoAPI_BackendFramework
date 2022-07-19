12c4108927c1f3ca5aac42c7547e7ff1dd3e80c5
chad <ccrsailors@gmail.com>
2022-07-19 17:39:37 -0500

    Migrated Data for Table + Created Enpoint to Find All Products

--- products/models.py
+++ products/models.py
@@ -6,3 +6,4 @@ class Products(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory_quantity = models.IntegerField()
--- products/views.py
+++ products/views.py
@@ -1,8 +1,29 @@
 from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Products
 
 @api_view(['GET'])
 def products_list(request):
+    products = Products.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
 
 
     return Response('ok')
