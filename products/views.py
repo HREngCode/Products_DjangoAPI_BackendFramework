d3b46ec906cee516e9aac5767d5f599365821948
chad <ccrsailors@gmail.com>
2022-07-19 19:04:30 -0500

    Created Endpoints for PUT and DELETE + Updated Product App URLs to Call the Product Detail function

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Products
+from products import serializers
 
 @api_view(['GET', 'POST'])
 def products_list(request):
@@ -17,20 +19,17 @@ def products_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
-
-
-    return Response('ok')
+    product = get_object_or_404(Products, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
