85ddfdc8dae8997f2fa781d5437432f1c06ce947
chad <ccrsailors@gmail.com>
2022-07-21 17:57:46 -0500

    Changed Model Name from Products to Product + Updated Line 19 in Views to return Status Code of 201

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
--- products/views.py
+++ products/views.py
@@ -3,25 +3,25 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
-from .models import Products
+from .models import Product
 from products import serializers
 
 @api_view(['GET', 'POST'])
-def products_list(request):
+def product_list(request):
     if request.method == 'GET':
-        products = Products.objects.all()
+        products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
-    product = get_object_or_404(Products, pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
         serializer = ProductSerializer(product);
         return Response(serializer.data)
